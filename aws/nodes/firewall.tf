779bfe78103772391c56a5f342cb8bd83d713a2f
Jam Risser <jam@codejam.ninja>
2019-10-10 01:54:18 -0500

    Updated firewall rules

--- aws/nodes/firewall.tf
+++ aws/nodes/firewall.tf
@@ -23,15 +23,21 @@ resource "aws_security_group" "node" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 2379
-    to_port     = 2379
+    from_port   = 2376
+    to_port     = 2376
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 2380
+    from_port   = 2379
     to_port     = 2380
     protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "UDP"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
@@ -41,9 +47,27 @@ resource "aws_security_group" "node" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "UDP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 10250
+    to_port     = 10252
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 10256
+    to_port     = 10256
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 30000
     to_port     = 32767
-    protocol    = "TCP"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
